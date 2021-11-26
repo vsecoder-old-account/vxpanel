c9ac05d5b53b9a4d15538090cf5492bd7dc75094
Всеволод <63463168+vsecoder@users.noreply.github.com>
2021-11-26 18:48:28 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-import uvicorn, os
+import uvicorn
 from fastapi import FastAPI
 from fastapi import Request
 from fastapi.staticfiles import StaticFiles
@@ -7,10 +7,13 @@ from starlette.templating import Jinja2Templates
 from starlette.responses import Response
 
 import os
-import json
 import psutil
 import datetime
 import platform
+import contextlib
+import time
+import socket
+#import speedtest
 
 # Create server
 app = FastAPI()
@@ -21,7 +24,9 @@ def stat():
     mem = psutil.virtual_memory()
     active_since = datetime.datetime.fromtimestamp(psutil.boot_time())
     cpu = psutil.cpu_percent()
-    
+    internet = psutil.net_io_counters()
+    #st = speedtest.Speedtest()
+
     status_cpu = 'normal'
     if cpu > 70: status_cpu = 'high'
     elif cpu > 30: status_cpu = 'middle'
@@ -36,21 +41,23 @@ def stat():
     if psutil.disk_usage('/').percent > 70: status_disk = 'high'
     elif psutil.disk_usage('/').percent > 30: status_disk = 'middle'
     else: status_disk = 'normal'
+
     data = {
+        'ip': socket.gethostbyname(socket.getfqdn()),
+        'uptime': datetime.datetime.now() - active_since,
+        'system': platform.system(),
+        'release': platform.release(),
+        'version': platform.version(),
         'cpu': {
             'used': psutil.cpu_count() / 100 * cpu,
             'free': psutil.cpu_count() - psutil.cpu_count() / 100 * cpu,
             'percent': cpu
         },
-        'uptime': (datetime.datetime.now() - active_since).days,
-        'system': platform.system(),
-        'release': platform.release(),
-        'version': platform.version(),
         'memory': {
             'total': round(mem.total / 1024 / 1024),
             'avaliable': round(mem.available / 1024 / 1024),
-            'used':round( mem.used / 1024 / 1024),
-            'free':round( mem.free / 1024 / 1024),
+            'used': round(mem.used / 1024 / 1024),
+            'free': round(mem.free / 1024 / 1024),
             'active': round(mem.active / 1024 / 1024),
             'inactive': round(mem.inactive / 1024 / 1024),
             'cached': round(mem.cached / 1024 / 1024),
@@ -68,9 +75,31 @@ def stat():
             'cpu': status_cpu,
             'memory': status_memory,
             'disk': status_disk
+        },
+        'internet': {
+            #'upload': st.upload(),
+            #'donwload': st.donwload(),
+            #'ping': st.ping(),
+            'bytes_sent': internet.bytes_sent,
+            'bytes_recv': internet.bytes_recv,
+            'packets_sent': internet.packets_sent,
+            'packets_recv': internet.packets_recv,
+            'errin': internet.errin,
+            'errout': internet.errout,
+            'dropin': internet.dropin,
+            'dropout': internet.dropout
         }
     }
     return data
+
+def test():
+    with open("test.txt", 'a') as fout, contextlib.redirect_stdout(fout):
+        psutil.test()
+    f = open("test.txt", 'r')
+    test = f.read()
+    f.close()
+    os.remove("test.txt")
+    return test
 
 # Server home page
 @app.get('/')
@@ -87,7 +116,23 @@ def panel_page(request: Request):
 
 @app.get('/logs')
 def logs_page(request: Request):
-    return templates.TemplateResponse('logs.html', {'request': request})
+    return templates.TemplateResponse('logs.html', {'request': request, 'data': test()})
+
+@app.get('/settings')
+def panel_page(request: Request):
+    return 'In dev'
+
+@app.get('/ssh')
+def panel_page(request: Request):
+    return 'In dev'
+
+@app.get('/api')
+def panel_page(request: Request):
+    return stat()
+
+@app.get('/test')
+def panel_page(request: Request):
+    return test()
 
 # Start server
 if __name__ == "__main__":
